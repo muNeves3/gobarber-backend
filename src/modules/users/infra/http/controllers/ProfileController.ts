9423d5d8b13c636496ac03aca518ef356b6d60fe
muNeves3 <murilogrilosn@gmail.com>
2020-12-06 16:00:29 -0300

    app

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,6 +1,6 @@
 import { Response, Request } from 'express';
-
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
@@ -14,9 +14,7 @@ export default class ProfileController {
 
     const user = await showProfileService.execute({ user_id });
 
-    delete user.password;
-
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
@@ -33,8 +31,6 @@ export default class ProfileController {
       old_password,
     });
 
-    delete user.password;
-
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
