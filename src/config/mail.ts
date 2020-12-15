f79363dfacf0f68e9f5614022e552ddfac52fb34
muNeves3 <murilogrilosn@gmail.com>
2020-12-15 18:43:41 -0300

    :books: implementing amazon s3

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,7 +1,20 @@
 interface IMailConfig {
-  driver: 'ethereal' | 'mailchimp';
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
 }
 
 export default {
   driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      email: 'murilodesouzaneves@gmail.com',
+      name: 'Murilo Neves',
+    },
+  },
 } as IMailConfig;
