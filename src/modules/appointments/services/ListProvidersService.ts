6f81f04ce1bda1d35cdb9c08f243b8de3a0616f4
muNeves3 <murilogrilosn@gmail.com>
2021-01-22 10:36:44 -0300

    :books: serialized cache

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,6 +1,7 @@
 // import path from 'path';
 // import fs from 'fs';
 import { inject, injectable } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 // import UploadConfig from '@config/upload';
 // import AppError from '@shared/errors/AppError';
@@ -28,12 +29,17 @@ class ListProvidersService {
       `providers-list:${user_id}`,
     );
 
+    // let users;
+
     if (!users) {
       users = await this.usersRepository.findAllProviders({
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;
